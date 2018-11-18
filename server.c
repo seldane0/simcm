b50c363b01da1e844b6659f2d206297087849b9c
blah <blah@blah.com>
2018-11-18 13:41:43 -0800

    Changes to create CM channel and ID.

--- server.c
+++ server.c
@@ -4,21 +4,34 @@
 #include <unistd.h>
 #include <rdma/rdma_cma.h>
 
-struct rdma_event_channel	*evt_ch;
+struct rdma_event_channel	*cm_ch;
+struct rdma_cm_id			*cm_id;
 
 int
 main(int argc, char *argv[])
 {
-	evt_ch = rdma_create_event_channel();
-	if (evt_ch == NULL) {
+	int ret;
+
+	/* Create CM channel. */
+	cm_ch = rdma_create_event_channel();
+	if (cm_ch == NULL) {
 		printf("Failed to allocate evt_ch.\n");
 		return -1;
 	}
-	printf ("evt_ch=%p fd=%d\n", evt_ch, evt_ch->fd);
+	printf ("cm_ch=%p fd=%d\n", cm_ch, cm_ch->fd);
+
+	/* Create CM ID ... roughtly equal to a socket. */
+	ret = rdma_create_id(cm_ch, &cm_id, NULL, RDMA_PS_TCP);
+	if (ret != 0) {
+		printf("Failed to create CM ID.\n");
+		return -1;
+	}
+	printf ("cm_id=%p\n", cm_id);
 
 
-	/* Destroy the event channel. */
-	rdma_destroy_event_channel(evt_ch);
+	/* Destroy the CM ID and Channel. */
+	rdma_destroy_id(cm_id);
+	rdma_destroy_event_channel(cm_ch);
 
 	return 0;
 }
